c9425bd7bd1e6c7a383dedd42f21c3ff5171ecb9
john lester escarlan <jlescarlan11@gmail.com>
2024-11-21 13:12:14 +0800

    saving in case of something went wrong.

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const BasicInfo = [
+  {
+    id: 0,
+    label: "First Name",
+    value: "",
+    placeholder: "Lester",
+    inputType: "input",
+  },
+  {
+    id: 1,
+    label: "Last Name",
+    value: "",
+    placeholder: "Escarlan",
+    inputType: "input",
+  },
+  {
+    id: 2,
+    label: "Professional title",
+    value: "",
+    placeholder: "Web Developer",
+    inputType: "input",
+  },
+  {
+    id: 3,
+    label: "Give a summary of yourself",
+    value: "",
+    placeholder: "Pogi",
+    inputType: "textarea",
+  },
+];
+
+export const Education = [
+  {
+    id: 0,
+    label: "School Name",
+    value: "",
+    placeholder: "XYZ University",
+    inputType: "input",
+  },
+  {
+    id: 1,
+    label: "Degree",
+    value: "",
+    placeholder: "BSc in Computer Science",
+    inputType: "input",
+  },
+  {
+    id: 2,
+    label: "Year Graduated",
+    value: "",
+    placeholder: "2020",
+    inputType: "input",
+  },
+];
+
+export const Experience = [
+  {
+    id: 0,
+    label: "Company Name",
+    value: "",
+    placeholder: "ABC Corp",
+    inputType: "input",
+  },
+  {
+    id: 1,
+    label: "Role",
+    value: "",
+    placeholder: "Software Engineer",
+    inputType: "input",
+  },
+  {
+    id: 2,
+    label: "Years of Experience",
+    value: "",
+    placeholder: "5",
+    inputType: "input",
+  },
+];
